27af6f696da203f59140038a0acfe8495c249a73
binarion <itmaster@duck.com>
2026-05-24 03:34:04 +0300

    feat: Add es, cjs & iife formats

---
+++ src/main.js
@@ -0,0 +1,42 @@
+// @ts-check
+
+/**
+ * @param {string} [selector='[data-placeholder-form]']
+ */
+export default function (selector = '[data-placeholder-form]') {
+   const forms = document.querySelectorAll(selector);
+   let placeholder = '\r';
+
+   for (const form of forms) {
+      form.addEventListener('focusin', focusInHandler);
+      form.addEventListener('focusout', focusOutHandler);
+
+      /**
+       * @param {Event} e
+       */
+      function focusInHandler(e) {
+         e.stopPropagation();
+         const target = /** @type {HTMLInputElement | HTMLTextAreaElement} */ (e.target);
+
+         if (target && target.hasAttribute('placeholder')) {
+            if (target.placeholder) {
+               [placeholder, target.placeholder] = [target.placeholder, placeholder];
+            }
+         }
+      }
+
+      /**
+       * @param {Event} e
+       */
+      function focusOutHandler(e) {
+         e.stopPropagation();
+         const target = /** @type {HTMLInputElement | HTMLTextAreaElement} */ (e.target);
+
+         if (target && target.hasAttribute('placeholder')) {
+            if (placeholder) {
+               [target.placeholder, placeholder] = [placeholder, target.placeholder];
+            }
+         }
+      }
+   }
+}
