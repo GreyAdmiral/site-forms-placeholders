a9181e0a1390d7488429453d843118074815b6d9
binarion <itmaster@duck.com>
2026-08-15 00:15:35 +0300

    refactor: Code refactoring

--- src/main.js
+++ src/main.js
@@ -10,31 +10,19 @@ export default function (selector) {
    let placeholder;
 
    for (const form of forms) {
-      form.addEventListener('focusin', focusInHandler);
-      form.addEventListener('focusout', focusOutHandler);
+      form.addEventListener('focusin', handler);
+      form.addEventListener('focusout', handler);
    }
 
    /**
     * @param {Event} e
     */
-   function focusInHandler(e) {
+   function handler(e) {
       e.stopPropagation();
       const target = /** @type {HTMLInputElement | HTMLTextAreaElement} */ (e.target);
 
       if (target && target.hasAttribute('placeholder')) {
          [placeholder = EMPTY_PLACEHOLDER, target.placeholder = EMPTY_PLACEHOLDER] = [target.placeholder, placeholder];
-      }
-   }
-
-   /**
-    * @param {Event} e
-    */
-   function focusOutHandler(e) {
-      e.stopPropagation();
-      const target = /** @type {HTMLInputElement | HTMLTextAreaElement} */ (e.target);
-
-      if (target && target.hasAttribute('placeholder')) {
-         [target.placeholder = EMPTY_PLACEHOLDER, placeholder = EMPTY_PLACEHOLDER] = [placeholder, target.placeholder];
       }
    }
 }
